1a8eabe9a483efc273819895e842e5e047c7cb58
iarabertopena <iarabertopena@gmail.com>
2026-06-08 18:00:28 -0300

    Update random_sampling.py

--- src/active_learning/random_sampling.py
+++ src/active_learning/random_sampling.py
@@ -1,1 +1,27 @@
+class RandomSampling(ActiveLearningStrategy):
 
+    def __init__(self, budget):
+
+        super().__init__()
+
+        self.budget = budget
+
+    def query(
+        self,
+        x=None,
+        y_true=None,
+        y_pred=None,
+        probabilities=None,
+        committee_predictions=None,
+        committee_probabilities=None
+    ):
+
+        self.total_seen += 1
+
+        if np.random.rand() < self.budget:
+
+            self.total_queried += 1
+
+            return True
+
+        return False
