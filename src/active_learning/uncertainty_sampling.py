35e7bda29529f51baddb907613df81297b215356
iarabertopena <iarabertopena@gmail.com>
2026-06-08 18:01:07 -0300

    Update uncertainty_sampling.py

--- src/active_learning/uncertainty_sampling.py
+++ src/active_learning/uncertainty_sampling.py
@@ -1,1 +1,44 @@
+def compute_uncertainty(probabilities):
+    uncertainties = []
 
+    for proba in probabilities:
+        # Least Confidence
+        confidence = np.max(proba)
+        uncertainty = 1 - confidence
+        uncertainties.append(uncertainty)
+
+    result = np.max(uncertainties)
+
+    return result
+
+class UncertaintySampling(ActiveLearningStrategy):
+
+    def __init__(self, threshold):
+
+        super().__init__()
+
+        self.threshold = threshold
+
+    def query(
+        self,
+        x=None,
+        y_true=None,
+        y_pred=None,
+        probabilities=None,
+        committee_predictions=None,
+        committee_probabilities=None
+    ):
+
+        self.total_seen += 1
+
+        uncertainty = compute_uncertainty(
+            probabilities
+        )
+
+        if uncertainty >= self.threshold:
+
+            self.total_queried += 1
+
+            return True
+
+        return False
